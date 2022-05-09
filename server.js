5706b4c757d0affe35eda78dca76793b27a0b7e8
unknown <jam319@protonmail.com>
2022-05-09 18:40:14 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const PORT = process.env.PORT
 const app = express()
 
 //MIDDLEWARE
-app.set('views', __dirname + '/views')
+app.set ('views', __dirname + '/views')
 app.set ('view engine', 'jsx')
 app.engine ('jsx', require ('express-react-views').createEngine())
+app.use (express.static('public'))
+
 //ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
@@ -17,6 +19,11 @@ app.get('/', (req, res) => {
 //BREADS
 const breadsController = require ('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })  
 
 //LISTEN
 app.listen (PORT, () => {

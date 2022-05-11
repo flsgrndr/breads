079ab3d0fad62919f6a6431f052505938d7d2d8f
unknown <jam319@protonmail.com>
2022-05-11 19:45:29 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -10,7 +10,6 @@ breads.get('/', (req, res) => {
             title: 'Index Page'
         }
     )
-   // res.send (Bread)
 })
 
 // NEW
@@ -42,9 +41,28 @@ breads.post('/', (req, res) => {
     }
     Bread.push(req.body)
     res.redirect('/breads')
-  }) 
+  })
 
-  // DELETE
+//EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+//UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayindex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`) 
+})
+
+ // DELETE
 breads.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
